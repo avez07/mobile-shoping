18b2601d78d178d0246905c38bab81d2d8fcf2e4
avez07 <ansariaavez12345@gmail.com>
2023-05-30 20:01:51 +0530

    updated

--- home-view/deals-view-page.php
+++ home-view/deals-view-page.php
@@ -16,56 +16,71 @@
 
 </style>
 <?php
-include "config.php";
+include "./config.php";
+// Pagination logic
+$sql_query = "SELECT * FROM user_deals_768";
+$sql_result = $conn->query($sql_query);
+$number_of_result = $sql_result->num_rows - 4;
 
-
-
-  //pagination logics started
-  $sql_query = "SELECT * FROM user_deals_768";
-  $sql_result = $conn->query($sql_query);
-  $number_of_result = ($sql_result->num_rows) -4 ;
-
-
-  function generateRandomNumber($min, $max) {
+function generateRandomNumber($min, $max) {
     return rand($min, $max);
 }
 
 // Check if random number is already set in session
+
+
 if (isset($_SESSION['random_number'])) {
-    $randomNumber = $_SESSION['random_number'];
+  $randomNumber = $_SESSION['random_number'];
 } else {
-    // Generate a new random number between 1 and 100 (change the values as per your requirement)
-    $randomNumber = generateRandomNumber(1, $number_of_result);
-    $_SESSION['random_number'] = $randomNumber;
+  // Generate a new random number between 1 and the number of results (excluding 4) from the query
+  $randomNumber = generateRandomNumber(1, $number_of_result);
+  $_SESSION['random_number'] = $randomNumber;
+ 
+}
+
+$result_per_page = 4;
+
+// Fetch data using the random number and result per page limit
+$sql_query_2 = "SELECT * FROM user_deals_768 LIMIT $randomNumber, $result_per_page";
+$sql_result_2 = $conn->query($sql_query_2);
+
+// Check if the query executed successfully
+if ($sql_result_2 === false) {
+  echo "Error executing query: " . $conn->error;
+} else {
+  // Check if there are rows returned
+  if ($sql_result_2->num_rows > 0) {
+      while ($row = $sql_result_2->fetch_assoc()) {
+          // Output HTML for each row of data
+          echo '<div class="col-lg-3 col-6">
+              <a href="deals-view.php" style="text-decoration: none;">
+                  <div class="card onhover">
+                      <img src="deals-image-upload/' . $row['FOOT_IMG'] . '" class="card-img-top high" alt="...">
+                      <div class="card-body">
+                          <div class="d-flex justify-content-between">
+                              <p class="card-text">
+                                  <span class="fs-6 bg-danger fw-semibold px-2 py-1 text-light">Up to ' . $row['DISCOUNT'] . ' off</span>
+                                  <span class="fs-6 text-danger ms-2 fw-semibold">Deal</span>
+                              </p>
+                              <p>
+                                  <span class="fw-semibold text-right">' . $row['PRICE'] . '</span>
+                              </p>
+                          </div>
+                          <p class="naow">' . $row['FOOTWEAR_NAME'] . '</p>
+                      </div>
+                  </div>
+              </a>
+          </div>';
+      }
+  } else {
+      echo "No data found.";
+  }
+
 }
 
 
-$result_per_page = 4;
-  
-$sql_query_2 = "SELECT * FROM user_deals_768 LIMIT " . $randomNumber . ',' . $result_per_page;
-$sql_result_2 = $conn->query($sql_query_2);
-  
-   if (!$sql_result_2) {
-    echo "NO DATA IS ADDED"  . $sql_query_2->error;
-  } else {
-    if ($sql_result_2->num_rows > 0) {
-      while ($row = $sql_result_2->fetch_assoc()) {
-echo '<div class="col-lg-3 col-6">
-          <a href = "deals-view.php" style = "text-decoration : none;"><div class="card onhover">
-            <img src="deals-image-upload/'.$row['FOOT_IMG'].'" class="card-img-top high" alt="...">
-            <div class="card-body">
-              <div class="d-flex justify-content-between">
-                <p class="card-text"><span class="fs-6 bg-danger fw-semibold px-2 py-1 text-light">Up to '.$row['DISCOUNT'].'
-                    off</span><span class="fs-6 text-danger ms-2 fw-semibold">Deal</span></p>
-                <p><span class="fw-semibold text-right">'.$row['PRICE'].'</span></p>
-              </div>
-              <p class ="naow">'.$row['FOOTWEAR_NAME'].'</p>
-            </div>
-          </div></a>
-        </div>';
-      }
-  } 
-  }
 
-
+  
+  
+ 
 ?>
--- partial/signup-add.php
+++ partial/signup-add.php
@@ -1,23 +1,31 @@
 <?php
 
 
-                           $showalert = false;
-                           if ($_SERVER ["REQUEST_METHOD"] == "POST") {
-                            include "../config.php";
-                               $name = $_POST["name"];
-                               $phone = $_POST["phone-number"];
-                               $email_id = $_POST["email"];
-                               $username = $_POST["user"];
-                               $address = $_POST["add"];
-                               $landmark = $_POST["land"];
-                               $city = $_POST["city"];
-                               $state = $_POST["state"];
-                               $zip = $_POST["zip"];
-                               $pass = $_POST["pass1"];
-                              
-                               
+$showalert = false;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    include "../config.php";
+    $name = $_POST["name"];
+    $phone = $_POST["phone-number"];
+    $email_id = $_POST["email"];
+    $username = $_POST["user"];
+    $address = $_POST["add"];
+    $landmark = $_POST["land"];
+    $city = $_POST["city"];
+    $state = $_POST["state"];
+    $zip = $_POST["zip"];
+    $pass = $_POST["pass1"];
 
-                               $conn -> query("CREATE TABLE IF NOT EXISTS shoe_user_5599(
+    // LOGICS FOR UNIQUE NAME OF USER THAT CREATED A TABLE IN THE WONDERS-ORDERS DATABASE
+    $str = $name;
+    $pattern = "/\s+/"; // Use a regular expression pattern to match one or more spaces
+    $replace = "_";
+    
+    $result = preg_replace($pattern, $replace, $str);
+  $table_name = uniqid($result."_" , true);
+
+
+
+    $conn->query("CREATE TABLE IF NOT EXISTS shoe_user_5599(
                                     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
                                  CUSTOMER_NAME varchar(30) NOT NULL,
                                  PHONE_NUMBER BIGINT NOT NULL,
@@ -29,19 +37,21 @@
                                  STATE_ varchar(30) NOT NULL,
                                  ZIP INT(10) NOT NULL,
                                   PASSWORD_1 varchar(20) NOT NUll,
+                                  TABLE_NAME VARCHAR(50) UNIQUE NOT NULL,
                                    DATE_TIME DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
                                       );");
 
-                               $query = $conn -> query("INSERT INTO shoe_user_5599(CUSTOMER_NAME ,  PHONE_NUMBER , EMAIL_ID , USER_NAME ,  ADDRESS_ ,  LANDMARK , CITY ,  STATE_ , ZIP , PASSWORD_1) VALUES ( '$name', '$phone' , '$email_id',  '$username', '$address' , '$landmark' , '$city' , '$state' , '$zip' , '$pass')");
-                              
-                              
-                               if (!$query) {
-                                   die("Error preparing SQL statement: " .$conn -> error);
-                               } else{
-                                header("location:../signup.php");
-                                $showalert = true;
-                               
-                               }
-                           };
-                          
-                       ?>
+    $query = $conn->query("INSERT INTO shoe_user_5599(CUSTOMER_NAME ,  PHONE_NUMBER , EMAIL_ID , USER_NAME ,  ADDRESS_ ,  LANDMARK , CITY ,  STATE_ , ZIP , PASSWORD_1, TABLE_NAME) VALUES ( '$name', '$phone' , '$email_id',  '$username', '$address' , '$landmark' , '$city' , '$state' , '$zip' , '$pass' ,'$table_name')");
+
+
+    if (!$query) {
+        die("Error preparing SQL statement: " . $conn->error);
+    } else {
+        header("location:../signup.php");
+        $showalert = true;
+
+    }
+}
+;
+
+?>
